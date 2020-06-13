cc9645fe41ceca9c4ac64ecbae5cf6cddef61cfa
Abhijeet0001 <64309773+Abhijeet0001@users.noreply.github.com>
2020-06-13 17:26:33 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+function setup() {
+  createCanvas(1200,1200);
+//  createSprite(400, 200, 50, 50);
+}
+
+function draw() {
+  background(255,255,255);  
+ // drawSprites();
+ rectMode(CENTER)
+ rect(100,1000,100,600)
+ triangle(50,700,150,700,100,500)
+ rect(900,1000,100,600)
+ triangle(850,700,950,700,900,500)
+ rect(250,1000,100,400)
+ rect(750,1000,100,400)
+ rect(500,1000,200,500)
+ triangle(400,750,600,750,500,500)
+}
